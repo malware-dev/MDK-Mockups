95cafc924a1398ac10b4e9a598b97070f773eda3
Kristian Williams <kristian@softtech.nl>
2019-04-26 15:06:57 +0200

    - Improved the in-game data scafolding system. - Moved font data extrapolation to runtime to reduce space.

---
+++ DataExtractor/MockTextSurface.template.cs
@@ -0,0 +1,29 @@
+﻿//%HEADER%
+using System.Collections.Generic;
+
+namespace IngameScript.Mockups.Base
+{
+	public partial class MockTextSurface
+	{
+
+        static readonly IReadOnlyDictionary<string, string> _fonts = new Dictionary<string, string>
+		{
+//%FONTS%
+        };
+                
+		static readonly IEnumerable<string> _textures = new List<string>
+		{
+//%TEXTURES%
+        };
+        
+		static readonly IEnumerable<string> _sprites = new List<string>
+		{
+//%SPRITES%
+        };
+        
+		static readonly IEnumerable<string> _scripts = new List<string>
+		{
+//%SCRIPTS%
+        };
+    }
+}
